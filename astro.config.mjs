264828cdfb3daa0c6aae2004efa9ffe7de84775a
Kudlicka Tomas <tomaskudlickawastaken@gmail.com>
2023-05-31 22:19:04 -0700

    feat: docker ci

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
 import solidJs from "@astrojs/solid-js";
 
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  integrations: [solidJs()]
+  integrations: [solidJs()],
+  adapter: node({
+    mode: "standalone"
+  })
 });
